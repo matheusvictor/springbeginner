7a50bf9c9f776956e3c829c7d19c857309352001
Matheus Victor <matheusvictor.salles@gmail.com>
2024-03-22 10:37:43 -0300

    feat: create product controller
    Available resources:
    - create a new product
    - list all products

---
+++ src/main/java/br/com/matheusvictor/springbeginner/controllers/ProductController.java
@@ -0,0 +1,39 @@
+package br.com.matheusvictor.springbeginner.controllers;
+
+import br.com.matheusvictor.springbeginner.dtos.ProductRecordDto;
+import br.com.matheusvictor.springbeginner.models.ProductModel;
+import br.com.matheusvictor.springbeginner.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> createProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        // A anotation @Valid valida o objeto de entrada, garantindo que os campos estão preenchidos corretamente
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public List<ProductModel> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+}
